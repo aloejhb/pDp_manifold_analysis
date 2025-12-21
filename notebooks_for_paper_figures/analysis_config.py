a00685659dac4313a862467fcd41d6ae4d2952b6
Bo Hu <bo.hu@fmi.ch>
2025-12-21 13:35:27 +0100

    Upload notebooks for paper figures

---
+++ notebooks_for_paper_figures/analysis_config.py
@@ -0,0 +1,48 @@
+juvenile_window_name_to_frames = {
+    'time_window_3s': [38, 63],
+    'time_window_5s': [38, 76],
+    'time_window_7s': [32, 85],
+}
+
+juvenile_distance_window_name_to_sample_size = {'time_window_3s': 70,
+                              'time_window_5s': 100,
+                              'time_window_7s': 100}
+juvenile_distance_window_name_to_seed = {'time_window_3s': 124,
+                       'time_window_5s': 8989,
+                       'time_window_7s': 8989}
+juvenile_capacity_window_name_to_M = {'time_window_3s': 70,
+                    'time_window_5s': 100,
+                    'time_window_7s': 140}
+juvenile_capacity_window_name_to_seed = {'time_window_3s': 2345,
+                       'time_window_5s': 9991,
+                       'time_window_7s': 8787}
+
+simulation_window_name_to_frames = {
+    'time_window_2s': [11, 29],
+}
+
+simulation_distance_window_name_to_sample_size = {'time_window_2s': 100}
+simulation_distance_window_name_to_seed = {'time_window_2s': 104}
+simulation_capacity_window_name_to_M = {'time_window_2s': 70}
+simulation_capacity_window_name_to_seed = {'time_window_2s': 2345}
+
+simulation_levels_of_corr_distance_window_name_to_sample_size = {'time_window_2s': 50}
+simulation_levels_of_corr_distance_window_name_to_seed = {'time_window_2s': 112}
+
+adult_window_name_to_frames = {
+    'time_window_3s': [47, 70],
+    'time_window_5s': [47, 85],
+    'time_window_7s': [40, 93],
+    'time_window_10s': [40, 115]
+}
+adult_distance_window_name_to_sample_size = {'time_window_3s': 70,
+                                             'time_window_5s': 100,
+                                             'time_window_7s': 100}
+adult_distance_window_name_to_seed = {'time_window_3s': 203,
+                                      'time_window_5s': 204,
+                                      'time_window_7s': 205}
+adult_capacity_N = 400
+adult_capacity_window_name_to_M = {'time_window_7s': 80,
+                                   'time_window_10s': 100}
+adult_capacity_window_name_to_seed = {'time_window_7s': 8887,
+                                      'time_window_10s': 8887}
