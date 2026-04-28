b46856165000f0ab8fa3336d53cc69bd080299b0
Bo Hu <bo.hu@fmi.ch>
2026-04-28 12:33:20 +0200

    offset response analysis

--- notebooks_for_paper_figures/analysis_config.py
+++ notebooks_for_paper_figures/analysis_config.py
@@ -21,16 +21,16 @@ simulation_window_name_to_frames = {
     'time_window_2s': [11, 29],
 }
 
-simulation_distance_window_name_to_sample_size = {'time_window_2s': 100}
-simulation_distance_window_name_to_seed = {'time_window_2s': 104}
-simulation_capacity_window_name_to_M = {'time_window_2s': 70}
-simulation_capacity_window_name_to_seed = {'time_window_2s': 2345}
+simulation_distance_window_name_to_sample_size = {'time_window_2s': 80}
+simulation_distance_window_name_to_seed = {'time_window_2s': 123}
+simulation_capacity_window_name_to_M = {'time_window_2s': 50}
+simulation_capacity_window_name_to_seed = {'time_window_2s': 123}
 
 simulation_levels_of_corr_distance_window_name_to_sample_size = {'time_window_2s': 50}
 simulation_levels_of_corr_distance_window_name_to_seed = {'time_window_2s': 112}
 
 adult_window_name_to_frames = {
-    'time_window_3s': [47, 70],
+    'time_window_3s': [47, 70], # For capacity time_window_3s was [47, 71]
     'time_window_5s': [47, 85],
     'time_window_7s': [40, 93],
     'time_window_10s': [40, 115]
@@ -42,7 +42,12 @@ adult_distance_window_name_to_seed = {'time_window_3s': 203,
                                       'time_window_5s': 204,
                                       'time_window_7s': 205}
 adult_capacity_N = 400
-adult_capacity_window_name_to_M = {'time_window_7s': 80,
+adult_capacity_window_name_to_M = {'time_window_3s': 70,
+                                   'time_window_5s': 70,
+                                   'time_window_7s': 80,
                                    'time_window_10s': 100}
-adult_capacity_window_name_to_seed = {'time_window_7s': 8887,
-                                      'time_window_10s': 8887}
+adult_capacity_window_name_to_seed = {
+                                    'time_window_3s': 8887,
+                                    'time_window_5s': 8887,
+                                    'time_window_7s': 8887,
+                                    'time_window_10s': 8887}
